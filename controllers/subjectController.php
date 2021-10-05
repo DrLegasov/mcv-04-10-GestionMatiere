b57e589398ea15337ec20b08060c995a9654c3e1
Christophe ROSSI <crossi@stagiaire-humanbooster.com>
2021-10-05 11:11:03 +0200

    dfg

--- controllers/subjectController.php
+++ controllers/subjectController.php
@@ -1,5 +1,10 @@
 <?php
-include('models/Subjects.php');
+require_once('models/MatClass.php');
+
+$oMatclass = new MatClass();
+$aSubject = $oMatclass->getListSubjet();
+
+
 include('views/subjectList.php');
 
 ?>
