d88ef671b377d2394ed7f5f2e4b9b90dceea4056
Christophe ROSSI <crossi@stagiaire-humanbooster.com>
2021-10-05 14:35:56 +0200

    constructor correct

--- controllers/sessionsController.php
+++ controllers/sessionsController.php
@@ -1,8 +1,10 @@
 <?php
 require_once('models/Session.php');
 
-$oSession = new Session();
+$oSession = new Session('XBR21-42','0000','0000','1');
 $Sessions = $oSession->getListSession();
+
+
 include('views/sessionsList.php');
 
 ?>
